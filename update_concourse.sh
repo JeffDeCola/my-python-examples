9ed9cf23dbfc716637b7a0f9a1c1cdac2db93849
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2017-03-08 11:17:03 -0800

    test git

--- update_concourse.sh
+++ update_concourse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# my-python-examples update_concourse.sh
+# my-python-examples update_concourse.sh 
 
 fly -t ci set-pipeline -p hello-go -c ci/pipeline.yml --load-vars-from ci/.credentials.yml
