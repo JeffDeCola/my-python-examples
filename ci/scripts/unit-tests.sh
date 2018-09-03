35e80ad57dcdc32727b343eca428f3abfba7dbda
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2018-09-02 17:27:58 -0700

    typo

--- ci/scripts/unit-tests.sh
+++ ci/scripts/unit-tests.sh
@@ -12,6 +12,7 @@ ls -lat
 # RUN unit_tests and it shows the percentage coverage
 # Print to stdout and file using tee
 echo "Placeholder to run python tests for my-python-examples" | tee test_coverage.txt
+
 # Add some whitespace to the begining of each line
 sed -i -e 's/^/     /' test_coverage.txt
 
